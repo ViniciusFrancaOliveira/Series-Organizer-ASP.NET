9b75ccffe6cca3c1d322a38b8cedd930e4eea597
vingadorv <vinifranca13@gmail.com>
2022-01-19 09:57:36 -0300

    initial commit

---
+++ SeriesOrganizer/SeriesOrganizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeriesOrganizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
